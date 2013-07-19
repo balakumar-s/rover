5edfe2f903d3d1c2c75947e41f98c100489397d3
balakumar-s <s.balakumar@outlook.com>
2013-07-20 00:19:16 +0530

    Rover Voice control alpha update

---
+++ build/CMakeFiles/voiceControlSpeech.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/voiceControlSpeech.dir/src/voiceControlSpeech.cpp.o"
+  "../bin/voiceControlSpeech.pdb"
+  "../bin/voiceControlSpeech"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/voiceControlSpeech.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
